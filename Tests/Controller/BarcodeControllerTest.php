a43e2ac3d7272706b23904ffa6333d8ba8dffe33
phiamo <phiamo@gmail.com>
2011-11-25 04:21:13 +0100

    added travis

---
+++ Tests/Controller/BarcodeControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Mopa\BootstrapBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BarcodeController extends WebTestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(True, 'Wow');
+    }
+}
